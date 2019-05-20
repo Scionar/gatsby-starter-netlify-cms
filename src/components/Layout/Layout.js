9eba4d08e39db2d0a0eb729e5098b979f94860b8
Joona Viertola <joona.viertola@houston-inc.com>
2019-05-20 12:08:07 +0300

    Fixed prop check for Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,23 @@
-import React from "react";
-import PropTypes from "prop-types";
-import classNames from "classnames";
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-import "./../../styles/base.scss";
-import "./Layout.scss";
+import './../../styles/base.scss';
+import './Layout.scss';
 
 const Layout = ({ children, modifier, ...props }) => {
   return (
-    <div className={classNames("layout", modifier)}>
+    <div className={classNames('layout', modifier)}>
       <div className="layout__content">{children}</div>
     </div>
   );
 };
 
 Layout.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.element.isRequired,
+    PropTypes.arrayOf(PropTypes.element).isRequired
+  ]),
   modifier: PropTypes.string
 };
 
