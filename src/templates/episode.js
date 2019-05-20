20e2e52a4308018f08720fc5593ff7a2d9292124
Joona Viertola <joona.viertola@houston-inc.com>
2019-05-20 15:23:37 +0300

    TwoCellGrid component

--- src/templates/episode.js
+++ src/templates/episode.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 
-import { Layout } from '../components';
+import { Layout, TwoCellGrid } from '../components';
 import { HeaderContainer, FooterContainer } from '../containers';
 
-const EpisodePage = ({ data }) => {
-  const node = data.feedAnchorFm;
+class EpisodePage extends Component {
+  constructor(props) {
+    super(props);
+    this.firstCell = this.firstCell.bind(this);
+    this.secondCell = this.secondCell.bind(this);
+  }
 
-  return (
-    <Layout>
-      <HeaderContainer />
-      <h1>{node.title}</h1>
-      <FooterContainer />
-    </Layout>
-  );
-};
+  firstCell() {
+    const node = this.props.data.feedAnchorFm;
+    return <div>{node.title}</div>;
+  }
+
+  secondCell() {
+    return <div>Content...</div>;
+  }
+
+  render() {
+    const node = this.props.data.feedAnchorFm;
+
+    return (
+      <Layout>
+        <HeaderContainer />
+        <TwoCellGrid
+          modifier="two-cell-grid--reverse"
+          firstCell={this.firstCell()}
+          secondCell={this.secondCell()}
+        />
+        <FooterContainer />
+      </Layout>
+    );
+  }
+}
 
 EpisodePage.propTypes = {
   data: PropTypes.shape({
