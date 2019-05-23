1b80c74870d6d5a002ddd97951c99df390b93db3
Joona Viertola <joona.viertola@houston-inc.com>
2019-05-23 12:59:50 +0300

    Added link to logo to landing page

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { Link } from 'gatsby';
 
 import './Logo.scss';
 import NPLogo from '../../img/np-logo.png';
 
-const Logo = () => <img className="logo" src={NPLogo} alt="Nudging Pixels" />;
+const Logo = ({ modifier }) => (
+  <Link className={classNames('logo', modifier)} to="/">
+    <img className="logo__image" src={NPLogo} alt="Nudging Pixels" />
+  </Link>
+);
+
+Logo.propTypes = {
+  modifier: PropTypes.string
+};
 
 export default Logo;
