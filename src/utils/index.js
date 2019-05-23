314bd5d437845115995f1f6027a1b5f8d06b2de1
Joona Viertola <joona.viertola@houston-inc.com>
2019-05-23 12:27:17 +0300

    Episode player URL transformer

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as episodePath } from './episode-path';
 export { default as stripHtml } from './strip-html';
 export { default as truncate } from './truncate';
+export { default as episodePlayerUrl } from './episode-player-url';
