a8569aea74743a5eaaa4c7546bbac04616561111
Joona Viertola <joona.viertola@houston-inc.com>
2019-05-27 14:10:39 +0300

    Added all episode available on -links

--- src/containers/EpisodeAvailableOnContainer.js
+++ src/containers/EpisodeAvailableOnContainer.js
@@ -14,12 +14,41 @@ class EpisodeAvailableOnContainer extends Component {
       {
         name: 'iTunes',
         link:
-          'https://podcasts.apple.com/fi/podcast/nudging-pixels/id1449180059'
+          'https://itunes.apple.com/us/podcast/nudging-pixels/id1449180059?mt=2&uo=4'
       },
       {
-        name: 'iTunes',
+        name: 'Google Podcasts',
         link:
-          'https://podcasts.apple.com/fi/podcast/nudging-pixels/id1449180059'
+          'https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy84N2I4MzA4L3BvZGNhc3QvcnNz'
+      },
+      {
+        name: 'Spotify',
+        link: 'https://open.spotify.com/show/5EYgxEZOKaYeCN2tKsDHvO'
+      },
+      {
+        name: 'Breaker',
+        link: 'https://www.breaker.audio/nudging-pixels'
+      },
+      {
+        name: 'Overcast',
+        link: 'https://overcast.fm/itunes1449180059/nudging-pixels'
+      },
+      {
+        name: 'Pocket Casts',
+        link: 'https://pca.st/4ydd'
+      },
+      {
+        name: 'PodBean',
+        link:
+          'https://www.podbean.com/podcast-detail/jxsuu-87146/Nudging-Pixels-Podcast'
+      },
+      {
+        name: 'RadioPublic',
+        link: 'https://radiopublic.com/nudging-pixels-6pw1yD'
+      },
+      {
+        name: 'Stitcher',
+        link: 'https://www.stitcher.com/podcast/anchor-podcasts/nudging-pixels'
       }
     ];
 
