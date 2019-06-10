c08af7259113af248c7d5a781ecbe82e1f7a89fa
Joona Viertola <joona.viertola@houston-inc.com>
2019-06-10 12:16:53 +0300

    Sort episodes to seasons in EpisodeFeedContainer

--- src/containers/EpisodeFeedContainer.js
+++ src/containers/EpisodeFeedContainer.js
@@ -3,31 +3,64 @@ import { StaticQuery, graphql } from 'gatsby';
 import _ from 'lodash';
 
 import { episodePath, stripHtml, truncate } from '../utils';
-import { EpisodeCard } from '../components';
+import { EpisodeCard, SeasonDropdown } from '../components';
 import examplePodcastCover from '../img/example-podcast-cover.jpg';
 
 const EpisodeFeedContainer = ({ data }) => {
   const episodes = data.allFeedAnchorFm.nodes;
-  return episodes.map((node, index) => {
-    // Set modifier class if index is uneven.
-    const modifier = index % 2 === 0 ? undefined : 'episode-card--poked-right';
-    const image = _.get(
-      node,
-      'localImage.childImageSharp.fluid.src',
-      undefined
-    );
+  const seasonArray = [];
 
+  episodes.map((node, index) => {
+    const season = node.itunes.season;
+    const episode = node.itunes.episode;
+    let seasonIndexInArray;
+
+    if (!Array.isArray(seasonArray[season])) {
+      seasonArray[season] = [];
+    }
+
+    if (seasonArray[season][episode]) {
+      console.log(
+        'Two episodes with same season and episode. Check Podcast feed to fix the problem.'
+      );
+    }
+
+    seasonArray[season][episode] = node;
+  });
+
+  return seasonArray.map((season, seasonIndex) => {
+    const dropdownOpen = seasonIndex === seasonArray.length - 1;
     return (
-      <EpisodeCard
-        key={node.guid}
-        modifier={modifier}
-        style={{ marginTop: '4rem' }}
-        runningNumber={node.itunes.episode}
-        coverImage={image}
-        title={node.title}
-        description={truncate(stripHtml(node.content), 150)}
-        link={episodePath(node.title)}
-      />
+      <SeasonDropdown
+        key={seasonIndex}
+        header={`Season ${seasonIndex}`}
+        style={{ marginTop: '2rem' }}
+        open={dropdownOpen}
+      >
+        {season.map((node, episodeIndex) => {
+          // Set modifier class if index is uneven.
+          const modifier =
+            episodeIndex % 2 === 0 ? undefined : 'episode-card--poked-right';
+          const image = _.get(
+            node,
+            'localImage.childImageSharp.fluid.src',
+            undefined
+          );
+
+          return (
+            <EpisodeCard
+              key={node.guid}
+              modifier={modifier}
+              style={{ marginTop: '2rem' }}
+              runningNumber={node.itunes.episode}
+              coverImage={image}
+              title={node.title}
+              description={truncate(stripHtml(node.content), 150)}
+              link={episodePath(node.title)}
+            />
+          );
+        })}
+      </SeasonDropdown>
     );
   });
 };
